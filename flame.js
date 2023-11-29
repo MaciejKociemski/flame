47d057b94d8035c1565b29d6145d9df34ce7b43a
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-11-29 11:31:59 +0100

    correct js

--- flame.js
+++ flame.js
@@ -1,34 +1,34 @@
-const wysokosc = 80;
-const szerokosc = 100;
+const h = 80;
+const w = 100;
 let fire = [];
 
-for (let i = 0; i < wysokosc * szerokosc; i++) fire[i] = 0;
+for (let i = 0; i < h * w; i++) fire[i] = 0;
 
 let intid = setInterval(flame, 50);
 let context = document.getElementById("myCanvas").getContext("2d");
 
 function flame() {
-  for (i = 0; i < szerokosc; i++)
-    fire[i + szerokosc] = Math.floor(Math.random() * 255);
+  for (i = 0; i < w; i++)
+    fire[i + w] = Math.floor(Math.random() * 255);
 
-  for (let y = wysokosc; y > 1; y--)
-    for (let x = 0; x < szerokosc; x++) {
-      i = y * szerokosc + x;
+  for (let y = h; y > 1; y--)
+    for (let x = 0; x < w; x++) {
+      i = y * w + x;
       fire[i] = Math.floor(
-        (fire[(y - 1) * szerokosc + ((x - 1 + szerokosc) % szerokosc)] +
-          fire[(y - 1) * szerokosc + ((x + szerokosc) % szerokosc)] +
-          fire[(y - 1) * szerokosc + ((x + 1 + szerokosc) % szerokosc)] +
-          fire[(y - 2) * szerokosc + ((x + szerokosc) % szerokosc)]) /
+        (fire[(y - 1) * w + ((x - 1 + w) % w)] +
+          fire[(y - 1) * w + ((x + w) % w)] +
+          fire[(y - 1) * w + ((x + 1 + w) % w)] +
+          fire[(y - 2) * w + ((x + w) % w)]) /
           4.04
       );
     }
 
-  for (i = szerokosc * 16; i < szerokosc * wysokosc; i++) {
+  for (i = w * 16; i < w * h; i++) {
     color = fire[i].toString(16);
     context.beginPath();
     context.rect(
-      (i % szerokosc) * 10,
-      (wysokosc - Math.floor(i / szerokosc)) * 10,
+      (i % w) * 10,
+      (h - Math.floor(i / w)) * 10,
       10,
       10
     );
