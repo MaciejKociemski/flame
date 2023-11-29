549c138de7ff8184a187f778292f3ee8ce2b98d9
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-11-29 17:24:48 +0100

    restructuring of the code

--- flame.js
+++ flame.js
@@ -1,18 +1,28 @@
 const h = 80;
 const w = 100;
 let fire = [];
+let intid;
+let context;
 
-for (let i = 0; i < h * w; i++) fire[i] = 0;
+function initialize() {
+  for (let i = 0; i < h * w; i++) {
+    fire[i] = 0;
+  }
 
-let intid = setInterval(flame, 50);
-let context = document.getElementById("myCanvas").getContext("2d");
+  intid = setInterval(flame, 50);
+  context = document.getElementById("myCanvas").getContext("2d");
+}
 
-function flame() {
-  for (i = 0; i < w; i++) fire[i + w] = Math.floor(Math.random() * 155);
+function generateRandomFire() {
+  for (let i = 0; i < w; i++) {
+    fire[i + w] = Math.floor(Math.random() * 155);
+  }
+}
 
-  for (let y = h; y > 1; y--)
+function updateFire() {
+  for (let y = h; y > 1; y--) {
     for (let x = 0; x < w; x++) {
-      i = y * w + x;
+      const i = y * w + x;
       fire[i] = Math.floor(
         (fire[(y - 1) * w + ((x - 1 + w) % w)] +
           fire[(y - 1) * w + ((x + w) % w)] +
@@ -21,12 +31,23 @@ function flame() {
           4.04
       );
     }
+  }
+}
 
-  for (i = w * 16; i < w * h; i++) {
-    color = fire[i].toString(16);
+function renderFire() {
+  for (let i = w * 16; i < w * h; i++) {
+    const color = fire[i].toString(16);
     context.beginPath();
     context.rect((i % w) * 10, (h - Math.floor(i / w)) * 10, 10, 10);
     context.fillStyle = "#" + color + "0000";
     context.fill();
   }
 }
+
+function flame() {
+  generateRandomFire();
+  updateFire();
+  renderFire();
+}
+
+initialize();
